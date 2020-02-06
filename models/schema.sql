86a3bfb81627073093df5335de5831bacdea6785
chylinski.t <chylinski.t@outlook.com>
2020-02-06 00:22:29 -0500

    restructured our html pages, added handlebars

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
+/*
 DROP DATABASE IF EXISTS healthfit;
 CREATE DATABASE healthfit;
 
 DROP DATABASE IF EXISTS healthfit;
 CREATE DATABASE healthfit;
+*/
+
+-- example for class activity delete after
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers_db
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
+
+)
