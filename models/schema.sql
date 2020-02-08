7eb3a4b0e1f38d1025c77edd2e4a07b09e07424d
chylinski.t <chylinski.t@outlook.com>
2020-02-08 13:09:33 -0500

    update app

--- models/schema.sql
+++ models/schema.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS healthfit;
-CREATE DATABASE healthfit;
 
+--Create Database 
+CREATE DATABASE healthfit;
 USE healthfit;
 
 
@@ -17,13 +18,3 @@ CREATE TABLE user_data
 );
 
 
-    INSERT INTO user_data (userId,userName,dailyCalorieGoal,goalWeight,currentWeight,age,gender)
-   	 VALUES (
-				1, "Dave Smith",2200, 200, 252,36,"Male"
-			),
-            (
-				2, "Steven Gerrard",1950, 195, 220,39,"Male"
-            ),
-            (   
-				3, "Keri Fowler",1820, 165, 179,29,"Female"
-            )
