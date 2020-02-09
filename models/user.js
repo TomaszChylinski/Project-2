fa2e1abdd191d0b77bb4b222b6b154009b647e9c
chylinski.t <chylinski.t@outlook.com>
2020-02-09 18:02:27 -0500

    app V2.0

--- models/user.js
+++ models/user.js
@@ -1,42 +1,46 @@
 module.exports = function(sequelize, Sequelize) {
-    var User = sequelize.define("user", {
-      userId: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-  
-      userName: {
-        type: Sequelize.STRING,
-        notEmpty: true
-      },
-  
-      dailyCalorieGoal: {
-        type: Sequelize.INTEGER,
-        notEmpty: true
-      },
-  
-      goalWeight: {
-        type: Sequelize.INTEGER,
-        notEmpty: true
-      },
-  
-      currentWeight: {
-        type: Sequelize.INTEGER,
-        notEmpty: true
-      },
-  
-      age: {
-        type: Sequelize.INTEGER,
-        notEmpty: true
-      },
-  
-      gender: {
-        type: Sequelize.STRING,
-        notEmpty: true
-      }
-    });
-  
-    return User;
-  };
-  
+  var User = sequelize.define("user", {
+    userId: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    userName: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+
+    dailyCalorieGoal: {
+      type: Sequelize.INTEGER,
+      notEmpty: true
+    },
+
+    goalWeight: {
+      type: Sequelize.INTEGER,
+      notEmpty: true
+    },
+
+    currentWeight: {
+      type: Sequelize.INTEGER,
+      notEmpty: true
+    },
+
+    age: {
+      type: Sequelize.INTEGER,
+      notEmpty: true
+    },
+
+    gender: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+
+    inspiration: {
+      type: Sequelize.STRING,
+      notEmpty: false
+    }
+  });
+
+  return User;
+};
