54682a3b16b4b5bd988dc914c5a1d800b0e4af8b
chylinski.t <chylinski.t@outlook.com>
2020-02-12 19:18:58 -0500

    edit nav

--- routes/form-api-routes.js
+++ routes/form-api-routes.js
@@ -9,16 +9,6 @@ module.exports = function(app) {
   });
 
 
-    app.get("/api/users", function(req, res) {
-    User.findAll({
-      where: {
-      },
-      order: [['createdAt', 'DESC']]
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
-
   // Create a new user
   app.post("/api/users", function(req, res) {
     db.User.create(req.body).then(function(dbUser) {
